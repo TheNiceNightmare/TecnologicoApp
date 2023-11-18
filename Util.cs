0c24cfb9ca6fa0a731a4574a2d2c8f1517189bc3
GizloDani <daniel.olivo@gizlocorp.com>
2023-11-18 17:09:22 -0600

    mi tarea

---
+++ Util.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace TecnologicoApp;
+
+public static class Util
+{
+    public static async Task ShowToastAsync(string message)
+    {
+        var toast = Toast.Make(message);
+        var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+        await toast.Show(cts.Token);
+    }
+}
