85229eb837ed338dbc6272aca66d22e8e45edf02
GizloDani <daniel.olivo@gizlocorp.com>
2023-11-24 14:46:16 -0500

    Actualización

---
+++ Models/Settings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TecnologicoApp.Models
+{
+    public static class Settings
+    {
+        public static bool IsAuthenticated
+        {
+            get => Preferences.Default.Get(nameof(IsAuthenticated), false);
+            set => Preferences.Default.Set(nameof(IsAuthenticated), value);
+        }
+
+        //public static bool IsAuthenticated { get; set; }
+    }
+}
